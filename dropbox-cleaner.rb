4874551986ad39026575d587c5fe2cf428fcfc84
Fabio Nisci <fabionisci@gmail.com>
2017-01-03 15:47:58 +0100

    new sha256

--- dropbox-cleaner.rb
+++ dropbox-cleaner.rb
@@ -2,7 +2,7 @@ class DropboxCleaner < Formula
   desc "Fix Dropbox conflicts automatically."
   homepage "https://github.com/fabiosoft/dropbox-cleaner"
   url "https://github.com/fabiosoft/dropbox-cleaner/archive/1.0.0.tar.gz"
-  sha256 "df6f22626d401f21cf4d72fdadbff761ca11676f921e174411caaec55ea7afbb"
+  sha256 "d4efffe159ec0a031da28d3182a03c7ecd670976afca0a80b16ea4a20a824ebc"
 
   def install
     bin.install "dropbox-cleaner"
