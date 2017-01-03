cf27de4cc9c4f4db340fe1fd16f93a1c07068c12
Fabio Nisci <fabionisci@gmail.com>
2017-01-03 15:57:28 +0100

    version 1.0.1

--- dropbox-cleaner.rb
+++ dropbox-cleaner.rb
@@ -1,8 +1,8 @@
 class DropboxCleaner < Formula
   desc "Fix Dropbox conflicts automatically."
   homepage "https://github.com/fabiosoft/dropbox-cleaner"
-  url "https://github.com/fabiosoft/dropbox-cleaner/archive/1.0.0.tar.gz"
-  sha256 "d4efffe159ec0a031da28d3182a03c7ecd670976afca0a80b16ea4a20a824ebc"
+  url "https://github.com/fabiosoft/dropbox-cleaner/archive/1.0.1.tar.gz"
+  sha256 "b520dd38c8c9cdae0a68f481aa8b122a1a892d943363e05ead4a0b1561c3f3ed"
 
   def install
     bin.install "dropbox-cleaner"
