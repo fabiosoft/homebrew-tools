73acc660c29c6feec4f24034a2acfabd33fd4541
Fabio Nisci <fabionisci@gmail.com>
2017-01-03 17:27:08 +0100

    no message

--- dropbox-cleaner.rb
+++ dropbox-cleaner.rb
@@ -4,8 +4,10 @@ class DropboxCleaner < Formula
   url "https://github.com/fabiosoft/dropbox-cleaner/archive/1.0.1.tar.gz"
   sha256 "b520dd38c8c9cdae0a68f481aa8b122a1a892d943363e05ead4a0b1561c3f3ed"
 
+  bottle :unneeded
+
   def install
-    bin.install "dropbox-cleaner-1.0.1/dropbox-cleaner"
+    bin.install "dropbox-cleaner-1.0.1/dropbox-cleaner.sh"
   end
 
   test do
